2c6072d97da4919e4b92c2dc6ca7ba8b6fcdb536
Jason Rabbitt <jrabbitt1110@gmail.com>
2016-04-22 08:55:14 -0600

    Just practicing, Arrow functions can't be used as constructors

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
 
+var Person = function(name, age) {
+	this.name = name
+	this.age = age
+}
+Person.prototype.print = function printPerson() {
+	console.log(`Name: ${this.name}\nAge: ${this.age}\n`)
+}
 
+var bob = new Person('Bob', 40)
+bob.print()
+
+var luigi = new Person('Luigi', 35)
+luigi.print()
+
+var link = new Person('Link', 18)
+link.print()
